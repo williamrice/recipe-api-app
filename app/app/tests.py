b3d9ccedfd10e180c7f48ebe54a498973571b939
William Rice <billyrice12@live.com>
2023-11-17 22:33:16 -0500

    added and tested dummy tests

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+    Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(calc.add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(calc.subtract(10, 15), 5)
